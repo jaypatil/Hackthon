b57107d420c8a63ed760b4b65597bd54f7bb66b1
jaypatil <jaypatil.sonu@gmail.com>
2016-03-13 10:26:45 +0530

    initial

---
+++ assets/assets/a/3/a3a5f6ec-0a38-4848-9b52-a4c6d335270b/fileAsset/main.js
@@ -0,0 +1,82 @@
+// User Info Flyoutv
+var userInfoDataLoaded = false;
+
+// User Info Flyout
+$(function () {
+    $("#userInfo .trigger").click(function () {
+        if($(this).parent().css("right") == "-335px"){
+            if(!userInfoDataLoaded){
+                $('#userInfoData').load('/application/ajax/location-clickstream.html');
+                userInfoDataLoaded=true;
+            };
+            $(this).parent().animate({right:'-10px'}, {queue: false, duration: 500});
+        }else{
+            $(this).parent().animate({right:'-335px'}, {queue: false, duration: 500});
+        }
+    })
+});
+
+function activeAddress(text) {
+    $('#address-title span').fadeOut('fast', function() {
+        $(this).text(text + 'Address');
+        $(this).fadeIn();
+    });
+}
+
+$(document).on('ready', function() {
+
+    var x;
+    x = $(window).height() - $(".header-navbar").height() - $(".footer-wrapper").height() - 50;
+    $(".content-wrap").css("min-height", x);
+
+    // Calling the Fancybox
+    $('.js-fancybox-img').fancybox({
+        type: 'image'
+    });
+
+    $('.fancybox-swf').fancybox({
+        type: 'swf'
+    });
+    
+    $('.js-fancybox-inline').fancybox({
+    	'transitionIn'	:	'elastic',
+		'transitionOut'	:	'elastic',
+		'speedIn'		:	600, 
+		'speedOut'		:	200 
+    });
+
+    $('.js-fancybox-media-link').fancybox({
+        openEffect  : 'none',
+        closeEffect : 'none',
+        helpers : {
+            media : {}
+        }
+    });
+
+    $('.js-fancybox-media-file').fancybox();
+
+    // CREATE ACCOUNT PAGE
+    // To change the heading text in the address section
+    $('.js-address-type .btn').on('click', function() {
+        text = $(this).text();
+        activeAddress(text);
+    });
+
+    // HOME CAROUSEL
+    $('#carousel-home').carousel({
+        'pause': 'hover'
+    });
+
+    // TABLE SORTER
+    $('.sorted-tables').tablesorter({
+        sortList: [[0,0]]
+    });
+
+    // CHOSEN for the product detail and the event page
+    $('.js-product-nav, .js-event-categories').chosen({
+        width: '100%'
+    });
+});
+
+// Video.js
+videojs.options.flash.swf = '/_bower_components/video.js/video-js.swf';
